112dda348badd20762d23e10cbae595d6bf6f69f
Yuheng <yuheng.wang@gmx.de>
2017-05-17 14:39:48 +0200

    to test

--- Tuersteuerung/DoorControl.cpp
+++ Tuersteuerung/DoorControl.cpp
@@ -8,17 +8,18 @@
 
 #include "DoorInterface.h"
 #include "DoorControl.h"
-#define closedoor 5
+#include <iostream>
+using namespace std;
+#define closetime 5
 
-bool abbruch = false;
 int modus = 0;
-int z_mode = 0;
-int z_mode_old = 0;
+int state_mode_new = 0;
+int state_mode_old = 0;
 bool zeit = false;
 bool vorgangoeffnen = false;
-bool abbruch = false;
-bool merkers = false;
-bool merkero = false;
+bool breakdown = false;
+//bool merkers = false;
+//bool merkero = false;
 
 struct in {
 	bool S1;
@@ -32,50 +33,50 @@ struct in {
 	bool LS2;
 	bool BE;
 	bool B;
-} eingaenge;
+} input;
 
 struct out {					//Ausgangsstruktur
 	bool Y1;
 	bool Y2;
 	bool Y3;
-} ausgaenge ;
+} output ;
 
-bool einlesen(){  					// eingabewerte einlesen und in Struktur einarbeiten
+bool senread(){  					// read sensor: eingabewerte einlesen und in Struktur einarbeiten
 	int ioinput;
-	int zaehler=0;
 	DoorInterface interface;
 	interface.DIO_Read(&ioinput);
-	eingaenge.S1=(ioinput & 1);
+
+	input.S1=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.S2=(ioinput & 1);
+	input.S2=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.E1=(ioinput & 1);
+	input.E1=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.E2=(ioinput & 1);
+	input.E2=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.X1=(ioinput & 1);
+	input.X1=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.X2=(ioinput & 1);
+	input.X2=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.LS1=(ioinput & 1);
+	input.LS1=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.X3=(ioinput & 1);
+	input.X3=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.LS2=(ioinput & 1);
+	input.LS2=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.BE=(ioinput & 1);
+	input.BE=(ioinput & 1);
 
 	ioinput= ioinput>>1;
-	eingaenge.B=(ioinput & 1);
+	input.B=(ioinput & 1);
 
 	ioinput= ioinput>>1;
 
@@ -85,88 +86,88 @@ bool einlesen(){  					// eingabewerte einlesen und in Struktur einarbeiten
 	return true;
 }
 
-bool ausgeben(){						//Ausgabewerte aus Struktur auslesen und an Funktion übergeben.
-	long int output=0;
-	output=ausgaenge.Y3;
-	output=output<<1;
-	output= output | ausgaenge.Y2;
-	output=output<<1;
-	output= output | ausgaenge.Y1;
+bool outwrite(){						//write output Ausgabewerte aus Struktur auslesen und an Funktion übergeben.
+	long int op = 0;
+	op = output.Y3;
+	op = op<<1;
+	op = op | output.Y2;
+	op = op<<1;
+	op = op | output.Y1;
 	DoorInterface interface;
-	interface.DIO_Write(output);
+	interface.DIO_Write(op);
 	return true;
 }
 
-int modi() {
-	int vorgang=0;
-	int zustand=0;
-	if (!eingaenge.S1 && !eingaenge.S2) // Automatikbetrieb
+int model() {
+	int vorgang = 0;
+	int state = 0;
+	if (!input.S1 && !input.S2) // Automatikbetrieb
 	{
 		vorgang++;
-		zustand = 0;
-		printf("Automatikbetrieb");
+		state = 0;
+		cout<<"Automatikbetrieb"<<endl;
 	}
-	if (!eingaenge.S1 && eingaenge.S2) // Handbetrieb
+	if (!input.S1 && input.S2) // Handbetrieb
 	{
 		vorgang++;
-		zustand = 1;
-		printf("Handbetrieb");
+		state = 1;
+		cout<<"Handbetrieb"<<endl;
 	}
-	if (eingaenge.S1 && !eingaenge.S2)	// Reparatur
+	if (input.S1 && !input.S2)	// Reparatur
 	{
 		vorgang++;
-		zustand=2;
-		printf("Reparatur");
+		state=2;
+		cout<<"Reparatur"<<endl;
 	}
-	if (eingaenge.S1 && eingaenge.S2) // PST_AUS
+	if (input.S1 && input.S2) // PST_AUS
 	{
 		vorgang++;
-		zustand = 3;
-		printf("Prozesssteuerung aus");
+		state = 3;
+		cout<<"Prozesssteuerung aus"<<endl;
 	}
 	if (vorgang>1)
 	{
 		return 127;
 	}
-	return zustand;
+	return state;
 }
 
 int betriebszustand () {
-	if (abbruch)
+	if (breakdown)
 	{
-		printf("Fehler! abbruch");
+		cout<<"Error"<<endl;
 	}
 	else {
 		if (modus==0)
 		{
-		if(eingaenge.BE && eingaenge.B && eingaenge.LS1 && eingaenge.LS2 && eingaenge.X3 && eingaenge.X2 && !eingaenge.X1 && !zeit){
+		if(input.BE&&input.B&&input.LS1&&input.LS2&&input.X3&&input.X2 && !input.X1 && !zeit){
 			return 1;//zeitveroegerung
 		}
-		if(eingaenge.BE && eingaenge.B && eingaenge.LS1 && eingaenge.LS2 && eingaenge.X3 && eingaenge.X2 && !eingaenge.X1 && zeit){
+		if(input.BE && input.B && input.LS1 && input.LS2 && input.X3 && input.X2 && !input.X1 && zeit){
 			return 2;//schliessen
 		}
-		if(eingaenge.BE && eingaenge.B && eingaenge.LS1 && eingaenge.LS2 && !eingaenge.X3 && !eingaenge.X2 && !vorgangoeffnen){
+		if(input.BE && input.B && input.LS1 && input.LS2 && !input.X3 && !input.X2 && !vorgangoeffnen){
 			return 3;//geschlossen
 		}
-		if(((!eingaenge.BE || !eingaenge.B || !eingaenge.LS1 || !eingaenge.LS2) && eingaenge.X1)||(vorgangoeffnen && eingaenge.X1)) {
+		if(((!input.BE || !input.B || !input.LS1 || !input.LS2) && input.X1)||(vorgangoeffnen && input.X1)) {
 			return 4;//oeffnen
 		}
-		if((!eingaenge.BE || !eingaenge.B || !eingaenge.LS1 || !eingaenge.LS2) && !eingaenge.X1) {
+		if((!input.BE || !input.B || !input.LS1 || !input.LS2) && !input.X1) {
 			return 5;//offen
 		}
 		return 6;
 		}
 		if (modus == 1){
-		if(!eingaenge.E1 && eingaenge.E2 && eingaenge.X3){
+		if(!input.E1 && input.E2 && input.X3){
 			return 2;//schliessen
 		}
-		if(eingaenge.E2 && !eingaenge.X3){
+		if(input.E2 && !input.X3){
 			return 3;//geschlossen
 		}
-		if( !eingaenge.E2 && eingaenge.X1){
+		if( !input.E2 && input.X1){
 			return 4;//oeffnen
 		}
-		if( !eingaenge.E2 && !eingaenge.X1){
+		if( !input.E2 && !input.X1){
 			return 5;//geoffnet
 		}
 		return 6; //fehlerfall
@@ -177,71 +178,71 @@ int betriebszustand () {
 }
 
 void steuern(){
-	printf("Betriebszustand: %d",z_mode);
+	printf("Betriebszustand: %d",state_mode_new);
 	DoorInterface interface;
-	switch (z_mode) {
+	switch (state_mode_new) {
 	case 0: {							//PST_AUS
-		ausgaenge.Y1=false;
-		ausgaenge.Y2=false;
-		ausgaenge.Y3=false;
-		bool test=ausgeben();
-		vorgangoeffnen=false;
-		zeit=false;
+		output.Y1 = 0;
+		output.Y2 = 0;
+		output.Y3 = 0;
+		bool test = outwrite();
+		vorgangoeffnen = 0;
+		zeit = 0;
 		break;
 	}
 	case 1: {							//zeitveroegerung
-		int h = interface.StartTimer(closedoor);
-		zeit=true;
+		int h = interface.StartTimer(closetime);
+		zeit = 1;
 		break;
 	}
 	case 2: {							//schliessen
-		ausgaenge.Y1=false;
-		ausgaenge.Y2=true;
-		ausgaenge.Y3=true;
-		bool test=ausgeben();
-		vorgangoeffnen=false;
+		output.Y1 = 0;
+		output.Y2 = 1;
+		output.Y3 = 1;
+		bool test = outwrite();
+		vorgangoeffnen = 0;
 		//zeit=false;
 		break;
 	}
 	case 3: {							//geschlossen
-		ausgaenge.Y1=false;
-		ausgaenge.Y2=false;
-		ausgaenge.Y3=false;
-		bool test=ausgeben();
-		vorgangoeffnen=false;
-		zeit=false;
+		output.Y1 = 0;
+		output.Y2 = 0;
+		output.Y3 = 0;
+		bool test = outwrite();
+		vorgangoeffnen = 0;
+		zeit = 0;
 		break;
 	}
 	case 4: {							//oeffnen
-		ausgaenge.Y1=true;
-		ausgaenge.Y2=false;
-		ausgaenge.Y3=false;
-		bool test=ausgeben();
-		zeit=false;
-		vorgangoeffnen=true;
+		output.Y1 = 1;
+		output.Y2 = 0;
+		output.Y3 = 0;
+		bool test = outwrite();
+		zeit = 0;
+		vorgangoeffnen = 1;
 		break;
 	}
 	case 5: {							//offen
-		ausgaenge.Y1=false;
-		ausgaenge.Y2=false;
-		ausgaenge.Y3=false;
-		bool test=ausgeben();
-		vorgangoeffnen=false;
-		zeit=false;
+		output.Y1 = 0;
+		output.Y2 = 0;
+		output.Y3 = 0;
+		bool test = outwrite();
+		vorgangoeffnen = 0;
+		zeit = 0;
 		break;
 	}
 	case 6: {							//fehler
-		ausgaenge.Y1=false;
-		ausgaenge.Y2=false;
-		ausgaenge.Y3=false;
-		bool test=ausgeben();
-		vorgangoeffnen=false;
-		printf("Fehler! Programmabbruch");
-		z_mode=0;
-		z_mode_old=0;
-		modus=0;
-		abbruch=true;
-		zeit=false;
+		output.Y1 = 0;
+		output.Y2 = 0;
+		output.Y3 = 0;
+		bool test = outwrite();
+		vorgangoeffnen = 0;
+		cout<<"Error"<<endl;
+		state_mode_new = 0;
+		state_mode_old = 0;
+		modus = 0;
+		breakdown = 1;
+		zeit = 0;
 		break;
 	}
 	}
@@ -279,13 +280,19 @@ int main (int argc, char *argv[])
     control.DoorControl();
     control.run();
 
-    while(!abbruch){
+    while(!breakdown){
 	    	//int timer = interface.StartTimer(0.05); //set Timer
-	    	bool okl = einlesen();
-	    	modus = modi();
-	    	abbruch = (okl);
-	    	z_mode = betriebszustand();
-	    	
+	    	bool readvalue = senread();
+	    	modus = model();
+	    	breakdown = (readvalue);
+	    	state_mode_new = betriebszustand();
+	    	if(state_mode_new!=state_mode_old){
+	    		steuern();
+	    	}
+	    	state_mode_old = state_mode_new;
+	    	if(breakdown){
+	    		cout<<"Error"<<endl;
+	    	}
 	    }
 	
 	return 0;
